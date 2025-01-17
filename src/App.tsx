acbd9108a959a09df30306e4f873d8c36ece1b25
xixin <zhengxixin@orvibo.com>
2025-01-17 18:39:59 +0800

    init page

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayoutBasic from "./components/dashboard_layout_basic";
+
+export const App: React.FC = () => {
+    return (
+        <DashboardLayoutBasic />
+    );
+};
